890487e364914f3fdf725ca0e4a65f9716d692f7
markhagiwara <lilac.hagikazu@gmail.com>
2020-03-30 20:35:18 -0700

    add about me, footer

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get "top" => "home#top"
+  get "/" => "home#top"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "about" => "home#about"
+  get "experience" => "home#experience"
 end
