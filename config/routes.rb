40643dc18639eb34187289a8d903c2d583591334
markhagiwara <lilac.hagikazu@gmail.com>
2020-04-03 21:28:04 -0700

    add education

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "about" => "home#about"
   get "experience" => "home#experience"
+  get "education" => "home#education"
 end
